f62954facb3bb17e1e5d43ebbe21fa9db61dbbf1
Poul Henriksen <pohe@easj.dk>
2019-03-04 10:28:25 +0100

    Add project files.

---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Sandbox
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            //bool ok = true; //lokal variabel
+
+            //if (ok)
+            //{
+            //    Console.WriteLine("Er ok");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("False");
+            //}
+
+            //int age = 17;
+            //Console.WriteLine("Starting to check age...");
+            //if (age > 12)
+            //{
+            //    if (age < 20)
+            //    {
+            //        Console.WriteLine("You are a teenager");
+            //    }
+            //}
+            //if ((age < 20) && (age > 12))  
+            //{
+            //    Console.WriteLine("You are a teenager");
+            //}
+            //int age = 15;
+            //string message = "You are ";
+
+            //if (age < 18)
+            //{
+            //    message = message + "a child.";
+            //}
+            //else
+            //{
+            //    message = message + "an adult.";
+            //}
+            //message = message + ((age < 18) ? "a child." : "an adult.");
+
+            //Console.WriteLine(message);
+            //int score = 60;
+            //if (score >= 90)
+            //{
+            //    Console.WriteLine("Mark is: A");
+            //}
+            //else if (score >= 70)
+            //{
+            //    Console.WriteLine("Mark is: B");
+            //}
+            //else if (score >= 40)
+            //{
+            //    Console.WriteLine("Mark is: C");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Mark is: D");
+            //}
+
+            //Console.WriteLine("Finished checking age");
+            //int noOfChildren = 2;
+            //int childSupport = 0; 
+            //switch (noOfChildren)
+            //{
+            //    case 0:
+            //        childSupport = 0;
+            //        break;
+            //    case 1:
+            //        childSupport = 1200;
+            //        break;
+            //    case 2:
+            //        childSupport = 2000;
+            //        break;
+            //    case 3:
+            //        childSupport = 2600;
+            //        break;
+            //    default:
+            //        childSupport = 3000;
+            //        break;
+            //}
+
+            //int number = 1;
+            //while (number < 5)
+            //{
+            //    Console.WriteLine("The value of the number is {0}", number);
+            //    number = number + 2;
+            //}
+
+            //int r = 0;
+
+            //while (r <10)
+            //{
+
+            //    int s = 0;
+            //    while (s < 10)
+            //    {
+            //        Console.Write("*");
+            //        s++;
+            //    }
+            //    Console.WriteLine();
+            //    r++;
+            //}
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
